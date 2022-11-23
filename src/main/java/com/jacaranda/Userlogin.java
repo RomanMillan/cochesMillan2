3f309e5c9447fa24bc6f6e174465cc017bd0c8a1
RomanMillan <romandos962m@gmail.com>
2022-11-23 22:04:03 +0100

    intento de arreglar sesiones

--- src/main/java/com/jacaranda/Userlogin.java
+++ src/main/java/com/jacaranda/Userlogin.java
@@ -28,113 +28,76 @@ public class Userlogin extends HttpServlet {
      */
     public Userlogin() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
+    //METODO DOGET
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// Comprobar que no estoy recibiendo ningun paraématro
 		// -sui no tengo pararmeto llamo a dopost y si tengo error
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		String nick = null;
-		String password = null;
-			HttpSession sesion = request.getSession();
-			nick = (String) sesion.getAttribute("user");
-			
-			out.println("<html>"
-					+ "<head>"
-					+ "<link rel='stylesheet' href='css/Main.css' type='text/css'>"
-					+ "</head>"
-					+ "<body>");
-			out.println("<header class='title'><h1>Bienvenido " + nick+"</h1>");
-			out.println("<h2>Lista de coches</h2>"
-					+ "<a href='/cochesMillan2/html/Index.html'>Atras</a>"
-					+ "</header>");
-			out.println("<table border='1px' align='center'>"
-					+ "<thead>"
-					+ "<tr>"
-					+ "<td>Modelo</td><td>Descripción</td><td>Precio</td><td>Cantidad</td> <td>Añadir</td>"
-					+ "</tr>"
-					+ "</thead>");
-
-			List<Element> el = ElementControl.getAllElements();
-			for(Element i: el) {
-				out.println("<tr><td>"+ i.getName()+"</td>");
-				out.println("<td>"+ i.getDescription()+"</td>");
-				out.println("<td>"+ i.getPrice()+" e</td>");
-				out.println("<form action='CartQuery' method='post'>");
-				out.println("<td hidden='true'><input name='idE' value='"+i.getId()+"'></td>");
-				out.println("<td><input name='quE'></td>");
-				out.println("<td><button type='submit'>Añadir</button></td></form></tr>");
-			}
+	
+		doPost(req,resp);
 			
 			
-			out.println("</table>");
-			out.println("</body></html>");
 			
 	}
 
+	String nick;
+	String password;
+	//METODO DOPOST
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String nick = null;
-		String password = null;
-		
-		if(nick != null || password != null) {
-			
-		}else {
-			
+		HttpSession sesion = request.getSession();
+
+//		if(sesion.getAttribute("login")==null && !request.getParameter("user").equals(nick)) {
+		if(sesion.getAttribute(nick) == null) {
+			nick = request.getParameter("user");
+			password = DigestUtils.md2Hex(request.getParameter("password"));
 		}
-		nick = request.getParameter("user");
-		password = DigestUtils.md2Hex(request.getParameter("password"));
+			if(UserControl.isValidUser(nick,password)) {
+				if(!UserControl.isAdministrator(nick,password)) {
+					sesion.setAttribute("login","true");
+					sesion.setAttribute("user",nick);
+					
+					out.println("<html>"
+							+ "<head>"
+							+ "<link rel='stylesheet' href='css/Main.css' type='text/css'>"
+							+ "</head>"
+							+ "<body>");
+					out.println("<header class='title'><h1>Bienvenido " + nick+"</h1>");
+					out.println("<h2>Lista de coches</h2>"
+							+ "<a href='/cochesMillan2/html/Index.html'>Atras</a> <input type='submit' value='cerrar sesión'>"
+							+ "</header>");
+					out.println("<table border='1px' align='center'>"
+							+ "<thead>"
+							+ "<tr>"
+							+ "<td>Modelo</td><td>Descripción</td><td>Precio</td><td>Cantidad</td> <td>Añadir</td>"
+							+ "</tr>"
+							+ "</thead>");
 		
-		
-		if(UserControl.isValidUser(nick,password)) {
-			if(!UserControl.isAdministrator(nick,password)) {
-				HttpSession sesion = request.getSession();
-				sesion.setAttribute("login","true");
-				sesion.setAttribute("user",nick);
-				
-				out.println("<html>"
-						+ "<head>"
-						+ "<link rel='stylesheet' href='css/Main.css' type='text/css'>"
-						+ "</head>"
-						+ "<body>");
-				out.println("<header class='title'><h1>Bienvenido " + nick+"</h1>");
-				out.println("<h2>Lista de coches</h2>"
-						+ "<a href='/cochesMillan2/html/Index.html'>Atras</a>"
-						+ "</header>");
-				out.println("<table border='1px' align='center'>"
-						+ "<thead>"
-						+ "<tr>"
-						+ "<td>Modelo</td><td>Descripción</td><td>Precio</td><td>Cantidad</td> <td>Añadir</td>"
-						+ "</tr>"
-						+ "</thead>");
-	
-				List<Element> el = ElementControl.getAllElements();
-				for(Element i: el) {
-					out.println("<tr><td>"+ i.getName()+"</td>");
-					out.println("<td>"+ i.getDescription()+"</td>");
-					out.println("<td>"+ i.getPrice()+" e</td>");
-					out.println("<form action='CartQuery' method='post'>");
-					out.println("<td hidden='true'><input name='idE' value='"+i.getId()+"'></td>");
-					out.println("<td><input name='quE'></td>");
-					out.println("<td><button type='submit'>Añadir</button></td></form></tr>");
+					List<Element> el = ElementControl.getAllElements();
+					for(Element i: el) {
+						out.println("<tr><td>"+ i.getName()+"</td>");
+						out.println("<td>"+ i.getDescription()+"</td>");
+						out.println("<td>"+ i.getPrice()+" e</td>");
+						out.println("<form action='CartQuery' method='post'>");
+						out.println("<td hidden='true'><input name='idE' value='"+i.getId()+"'></td>");
+						out.println("<td><input type='number' name='quE'></td>");
+						out.println("<td><button type='submit'>Añadir</button></td></form></tr>");
+					}
+					
+					out.println("</table>");
+					out.println("</body></html>");
+				}else {
+					response.sendRedirect("html/CreateElement.html");			
 				}
-				
-				
-				out.println("</table>");
-				out.println("</body></html>");
 			}else {
-				response.sendRedirect("html/CreateElement.html");			
+				response.sendRedirect("html/ErrorUser.html");	
 			}
-		}else {
-			response.sendRedirect("html/ErrorUser.html");	
-		}
-		
+
 	}
 
 }
